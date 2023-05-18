816e548fc6a67a98725aceb68a1592547339b24f
Shir Cohen <48015287+ShirCohen1@users.noreply.github.com>
2023-05-18 15:37:55 +0300

    Update train.py

--- mmdet/apis/train.py
+++ mmdet/apis/train.py
@@ -243,4 +243,4 @@ def train_detector(model,
         runner.resume(cfg.resume_from)
     elif cfg.load_from:
         runner.load_checkpoint(cfg.load_from)
-    return runner.run(data_loaders, cfg.workflow)
+    runner.run(data_loaders, cfg.workflow)
